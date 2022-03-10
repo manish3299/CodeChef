b106253c96328852e5e77c626d291dffe0eb543a
Manish Ambarwar <97036992+manish3299@users.noreply.github.com>
2022-03-10 15:37:54 +0530

    PROBCAT
    Chef prepared a problem. The admin has rated this problem for x points.
    
    A problem is called :
    
    1) Easy if 1≤x<100
    
    2) Medium if 100≤x<200
    
    3) Hard if 200≤x≤300
    
    Find the category to which Chef’s problem belongs.

---
+++ probcat.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x,t;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    if(x>=1 && x<100)
+	    {
+	      cout<<"Easy"<<endl;  
+	    }
+	    else if(x>=100 && x<200)
+	    {
+	       cout<<"Medium"<<endl;   
+	    }
+	    else
+	    {
+	       cout<<"Hard"<<endl;   
+	    }
+	}
+	
+
+}
